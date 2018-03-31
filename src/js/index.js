0173ae84e567cebe40058b0e8a48de4bf9f8d5fc
MindeVieras <wieraz21@gmail.com>
2018-03-31 04:33:38 +0100

    material ui added

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,26 @@
 
 import React from 'react'
 import { render } from 'react-dom'
+import { grey } from 'material-ui/styles/colors'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 import App from './App'
 
-render(<App />, document.getElementById('root'))
+const muiTheme = getMuiTheme({
+    fontFamily: 'Roboto',
+    palette: {
+        textColor: grey
+    },
+    appBar: {
+        height: 50
+    }
+})
+ 
+const DeployerApp = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+    <App />
+  </MuiThemeProvider>
+);
+
+render(<DeployerApp />, document.getElementById('root'))
